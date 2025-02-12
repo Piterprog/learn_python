a108fd232fc39df352d95bbe6870db215ca6b09e
Piter <152597719+Piterprog@users.noreply.github.com>
2025-02-12 06:00:12 +0000

    pull request

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def devision(a, b):
+    if b == 0:
+        return "Error you can devison to 0"
+    return a / b
+
+print("","-" *30, "\n |  Hello how i can help you  |" *1 ,"\n","-" *30)
+
